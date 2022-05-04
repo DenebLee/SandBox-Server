b411cfb3970c6de78e7fcb5f9aab0f23fb990f9b
lgsgst5613 <lgsgst5613@gmail.com>
2022-05-04 17:15:38 +0900

    Create Base SandBox Server

---
+++ src/main/java/kr/nanoit/MainTest.java
@@ -0,0 +1,21 @@
+package kr.nanoit;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Dto dto = new Dto("nanoit");
+        System.out.println(dto.getUsername());;
+    }
+
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    static class Dto {
+        private final String username;
+    }
+
+
+}
